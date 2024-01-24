9f40bd193e8cc2b4a9bce7998da1e026fae05e0b
Gaurav-Krishna-Gaali <gaali.gk19@gmail.com>
2024-01-25 03:40:22 +0530

    celery inint

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from config import DevelopmentConfig
 from application.resources import api
 from flask_security import Security, SQLAlchemyUserDatastore
 from application.sec import datastore
+from application.worker import celery_init_app
 
 def create_app():
     app = Flask(__name__)
@@ -17,6 +18,7 @@ def create_app():
     return app
 
 app = create_app()
+celery_app = celery_init_app(app)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
